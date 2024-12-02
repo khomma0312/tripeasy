ff2372565b3c2a7de54f0341f2d7e1448d6aa4f2
khomma0312 <kijikuroneko@gmail.com>
2024-12-02 23:36:59 +0900

    feat: NextAuth用の環境変数を追加

--- types/env.ts
+++ types/env.ts
@@ -4,6 +4,8 @@ declare global {
       NEXT_PUBLIC_APP_URL: string;
       SITE_NAME: string;
       DATABASE_URL: string;
+      GOOGLE_CLIENT_ID: string;
+      GOOGLE_CLIENT_SECRET: string;
     }
   }
 }
