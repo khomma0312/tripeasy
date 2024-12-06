c48d124ed2781ac04acb1f0ade7c6d2599c15db8
khomma0312 <kijikuroneko@gmail.com>
2024-12-07 07:54:14 +0900

    feat: emailカラムにUNIQUE制約を追加

--- schema/users.ts
+++ schema/users.ts
@@ -4,7 +4,7 @@ import { timestamps } from "@/lib/drizzle/columns-helpers";
 export const users = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity({ startWith: 1 }),
   name: varchar({ length: 256 }).notNull(),
-  email: varchar({ length: 256 }).notNull(),
+  email: varchar({ length: 256 }).notNull().unique(),
   password: varchar({ length: 256 }).notNull(),
   ...timestamps,
 });
