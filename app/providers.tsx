5c45042a2a650f4f5b8f6f990fefc805df413751
khomma0312 <kijikuroneko@gmail.com>
2024-12-07 07:46:06 +0900

    feat: shadcnのトーストコンポーネントを追加

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,14 +3,18 @@
 import { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "@/components/shadcn/toaster";
 
 const queryClient = new QueryClient();
 
 export const RootProviders = ({ children }: { children: ReactNode }) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+      <Toaster />
+    </>
   );
 };
