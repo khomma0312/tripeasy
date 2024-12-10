042f9ce8d6dc2fd24fa60c5b30a7de7d324d53bb
khomma0312 <kijikuroneko@gmail.com>
2024-12-10 22:57:40 +0900

    feat: メール認証用の画面を作成

---
+++ features/verify-email/components/email-verification-error/index.tsx
@@ -0,0 +1,29 @@
+import { Link } from "lucide-react";
+import { TitleHeading } from "@/components/shared/title-heading";
+import { paths } from "@/consts/common";
+import { PostVerifyEmail400, PostVerifyEmail404 } from "@/services/api/model";
+import { AxiosError } from "axios";
+
+type Props = { error: AxiosError<PostVerifyEmail400 | PostVerifyEmail404> };
+
+export const EmailVerificationError = ({ error }: Props) => {
+  return (
+    <div className="h-full grid grid-rows-[1fr_200px]">
+      <div className="flex flex-col items-center gap-4">
+        <TitleHeading>メールの検証に失敗しました😢</TitleHeading>
+        <div>
+          <p>{error?.response?.data.message}</p>
+          <p>他の登録方法もお試しください。</p>
+        </div>
+      </div>
+      <div>
+        <Link
+          href={paths.register.pathname}
+          className="text-primary hover:text-primary/65"
+        >
+          ←新規登録画面に戻る
+        </Link>
+      </div>
+    </div>
+  );
+};
---
+++ features/verify-email/components/email-verification-success/index.tsx
@@ -0,0 +1,25 @@
+import { TitleHeading } from "@/components/shared/title-heading";
+import { paths } from "@/consts/common";
+import { Link } from "lucide-react";
+
+export const EmailVerificationSuccess = () => {
+  return (
+    <div className="h-full grid grid-rows-[1fr_200px]">
+      <div className="flex flex-col items-center gap-4">
+        <TitleHeading>メールの検証に成功しました🎉</TitleHeading>
+        <div className="text-center">
+          <p>メールの検証が完了しました！</p>
+          <p>ログインして旅行の計画を立てましょう🧳</p>
+        </div>
+      </div>
+      <div className="flex justify-end">
+        <Link
+          href={paths.login.pathname}
+          className="text-primary hover:text-primary/65"
+        >
+          ログインページへ→
+        </Link>
+      </div>
+    </div>
+  );
+};
