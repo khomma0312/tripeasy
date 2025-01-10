f747c2d011f02287180d0d75be61eeeb35ca8ad5
khomma0312 <kijikuroneko@gmail.com>
2025-01-10 23:46:25 +0900

    clean: インポートの位置を修正

--- app/api/[...route]/route.ts
+++ app/api/[...route]/route.ts
@@ -1,18 +1,20 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import todoLists from "./todo-lists";
-import todoItems from "./todo-items";
 import register from "./register";
 import verifyEmail from "./verify-email";
 import requestToken from "./request-token";
+import todoLists from "./todo-lists";
+import todoItems from "./todo-items";
+import accommodations from "./accommodations";
 
 const app = new Hono().basePath("/api");
 
-app.route("/todo-lists", todoLists);
-app.route("/todo-items", todoItems);
 app.route("/register", register);
 app.route("/verify-email", verifyEmail);
 app.route("/request-token", requestToken);
+app.route("/todo-lists", todoLists);
+app.route("/todo-items", todoItems);
+app.route("/accommodations", accommodations);
 
 export const GET = handle(app);
 export const POST = handle(app);
