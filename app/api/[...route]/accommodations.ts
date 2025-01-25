5889788a1d86769d65514e77bd4829cc6d8f3712
khomma0312 <kijikuroneko@gmail.com>
2025-01-25 11:41:19 +0900

    feat: 画像アップロード、緯度経度変換含めたデータ登録のバックエンド処理を追加

--- app/api/[...route]/accommodations.ts
+++ app/api/[...route]/accommodations.ts
@@ -1,20 +1,134 @@
+import { File } from "buffer";
 import { Hono } from "hono";
+import { put } from "@vercel/blob";
 import {
+  accommodationInputSchema,
   ApiAllGetOutputType,
   ApiGetOutputType,
+  ApiPostOutputType,
 } from "@/lib/zod/schema/accommodations";
 import { db } from "@/lib/drizzle/db";
-import { accommodations as accommodationsTable, trips } from "@/schema";
-import { and, count, eq } from "drizzle-orm";
+import { accommodations as accommodationsTable } from "@/schema";
+import { and, count, desc, eq } from "drizzle-orm";
 import { getLogger } from "@/lib/logger";
 import { auth } from "@/lib/auth";
 import { ApiErrorType } from "@/lib/zod/schema/common";
 import { paginationDefaultLimit } from "@/consts/common";
-// import { zValidator } from "@hono/zod-validator";
+import { zValidator } from "@hono/zod-validator";
+import { getLatLngFromAddress } from "@/services/api/server";
 
 const logger = getLogger("api/accommodations");
 
 const app = new Hono()
+  .post("/", zValidator("form", accommodationInputSchema), async (c) => {
+    const session = await auth();
+    const {
+      name,
+      address,
+      checkIn,
+      checkOut,
+      reservationPrice,
+      notes,
+      image,
+      phoneNumber,
+      bookingId,
+      bookingUrl,
+      tripAdvisorUrl,
+      tripId,
+    } = c.req.valid("form");
+    let url: string | null = null;
+    let latLng: { x: number; y: number } | null = null;
+
+    if (!session?.user) {
+      logger.error("User verification failed.");
+      return c.json<ApiErrorType>(
+        { message: "ユーザー認証されていません" },
+        403
+      );
+    }
+
+    const userId = session.user.id ?? "";
+
+    // reservationPriceはintegerで保存するので数値に変換
+    const reservationPriceNum = Number(reservationPrice);
+
+    if (isNaN(reservationPriceNum)) {
+      logger.error("reservationPrice should not be NaN.");
+      return c.json<ApiErrorType>(
+        { message: "予約料金に数字以外の値が入力されています" },
+        400
+      );
+    }
+
+    // 住所が存在すれば、Google Maps APIを使って緯度経度の算出をする
+    if (address) {
+      try {
+        const { lat, lng, isLocationReliable } = await getLatLngFromAddress(
+          address
+        );
+        if (isLocationReliable) latLng = { x: lng, y: lat };
+      } catch (e) {
+        logger.error(e);
+      }
+    }
+
+    if (image instanceof File) {
+      try {
+        logger.info("Uploading image...");
+        const { url: uploadedUrl } = await put(
+          `/accomodations/${userId}/${image.name}`,
+          image,
+          {
+            access: "public",
+          }
+        );
+        url = uploadedUrl;
+
+        logger.info("Image uploaded successfully.");
+      } catch (e) {
+        logger.error(e);
+        return c.json<ApiErrorType>(
+          {
+            message: "ファイルのアップロードに失敗しました",
+          },
+          500
+        );
+      }
+    }
+
+    try {
+      const [accommodation] = await db
+        .insert(accommodationsTable)
+        .values({
+          name,
+          address: address || null,
+          checkIn,
+          checkOut,
+          reservationPrice:
+            reservationPriceNum === 0 ? null : reservationPriceNum,
+          notes: notes || null,
+          image: url,
+          phoneNumber: phoneNumber || null,
+          bookingUrl: bookingUrl || null,
+          tripAdvisorUrl: tripAdvisorUrl || null,
+          latLng,
+          tripId:
+            !isNaN(Number(tripId)) && Number(tripId) > 0
+              ? Number(tripId)
+              : null, // tripIdがfalsyでないかつ、NaNにならない場合はnumberに変換して登録
+          bookingId: bookingId || null,
+          userId,
+        })
+        .returning({ id: accommodationsTable.id });
+      return c.json<ApiPostOutputType>({ id: accommodation.id });
+    } catch (e) {
+      logger.error(e);
+      return c.json<ApiErrorType>(
+        { message: "データの登録に失敗しました" },
+        500
+      );
+    }
+  })
   .get("/", async (c) => {
     const session = await auth();
     const page = c.req.query("page") ? Number(c.req.query("page")) : 1;
@@ -48,7 +162,8 @@ const app = new Hono()
       .from(accommodationsTable)
       .where(eq(accommodationsTable.userId, userId))
       .offset(offset)
-      .limit(limit);
+      .limit(limit)
+      .orderBy(desc(accommodationsTable.id));
 
     const [totalCount] = await db
       .select({
@@ -62,6 +177,7 @@ const app = new Hono()
     return c.json<ApiAllGetOutputType>({
       accommodations: accommodations.map((accommodation) => ({
         ...accommodation,
+        image: accommodation.image ?? undefined,
         address: accommodation.address ?? undefined,
         bookingUrl: accommodation.bookingUrl ?? undefined,
         bookingId: accommodation.bookingId ?? undefined,
@@ -103,6 +219,8 @@ const app = new Hono()
     return c.json<ApiGetOutputType>({
       accommodation: {
         ...accommodation,
+        tripId: accommodation.tripId ?? undefined,
+        image: accommodation.image ?? undefined,
         address: accommodation.address ?? undefined,
         reservationPrice: accommodation.reservationPrice ?? undefined,
         notes: accommodation.notes ?? undefined,
--- types/env.ts
+++ types/env.ts
@@ -9,6 +9,8 @@ declare global {
       RESEND_FROM: string;
       RESEND_API_KEY: string;
       TRIP_ADVISOR_API_KEY: string;
+      BLOB_READ_WRITE_TOKEN: string;
+      GOOGLE_MAPS_API_KEY: string;
     }
   }
 }
