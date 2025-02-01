99e3e973cbd3e1f25ee128cd71ab4ef8115684fc
khomma0312 <kijikuroneko@gmail.com>
2025-02-02 00:33:08 +0900

    feat: 宿泊施設情報更新画面のロジック部分追加

--- features/accommodations/components/accommodation-edit-form-container/index.tsx
+++ features/accommodations/components/accommodation-edit-form-container/index.tsx
@@ -4,6 +4,8 @@ import { useGetTripsSuspense } from "@/services/api/endpoints/trips/trips";
 import { AccommodationForm } from "../accommodation-form";
 import { convertDataTypesToMatchTrips } from "@/features/trips/utils";
 import { useGetAccommodationsIdSuspense } from "@/services/api/endpoints/accommodations/accommodations";
+import { useAccommodationForm } from "../../hooks/use-accommodation-form";
+import { useEditAccommodation } from "../../hooks/use-edit-accommodation";
 
 type Props = {
   id: number;
@@ -12,8 +14,18 @@ type Props = {
 export const AccommodationEditFormContainer = ({ id }: Props) => {
   const { data: tripData } = useGetTripsSuspense({ page: 1, perPage: -1 });
   const { data } = useGetAccommodationsIdSuspense(id);
+  const { form } = useAccommodationForm(data.accommodation);
+  const { isPending, onSubmit } = useEditAccommodation(id, form);
 
   const trips = convertDataTypesToMatchTrips(tripData);
 
-  return <AccommodationForm trips={trips} defaultValues={data.accommodation} />;
+  return (
+    <AccommodationForm
+      formTitle="宿泊施設情報の編集"
+      trips={trips}
+      form={form}
+      isPending={isPending}
+      onSubmit={onSubmit}
+    />
+  );
 };
---
+++ features/accommodations/hooks/use-edit-accommodation.ts
@@ -0,0 +1,56 @@
+import { UseFormReturn } from "react-hook-form";
+import { useQueryClient } from "@tanstack/react-query";
+import { useErrorToast } from "@/hooks/common/use-error-toast";
+import { useToast } from "@/hooks/shadcn/use-toast";
+import { usePatchAccommodationsId } from "@/services/api/custom/endpoints/accommodations/patch";
+import { format } from "date-fns";
+import { dateFormatStrForParse } from "@/consts/common";
+import { AccommodationFormFieldValues } from "../types";
+
+export const useEditAccommodation = (
+  id: number,
+  form: UseFormReturn<AccommodationFormFieldValues>
+) => {
+  const { toast } = useToast();
+  const { errorToast } = useErrorToast();
+  const queryClient = useQueryClient();
+
+  const { isPending, mutate } = usePatchAccommodationsId({
+    mutation: {
+      onSuccess: () => {
+        toast({ title: "宿泊施設情報を変更しました" });
+        queryClient.invalidateQueries({
+          queryKey: [`/api/accommodations/${id}`, `/api/accommodations`],
+        });
+      },
+      onError: () => {
+        errorToast("宿泊施設情報の変更に失敗しました");
+      },
+    },
+  });
+
+  const editAccommodation = (accommodation: AccommodationFormFieldValues) => {
+    // mutateの引数の型に合わせるため、型を変換
+    const typeConvertedAccommodation = {
+      ...accommodation,
+      checkIn: format(accommodation.checkIn, dateFormatStrForParse),
+      checkOut: format(accommodation.checkOut, dateFormatStrForParse),
+    };
+
+    mutate({
+      id,
+      data: {
+        accommodation: typeConvertedAccommodation,
+      },
+    });
+  };
+
+  const onSubmit = (accommodation: AccommodationFormFieldValues) => {
+    editAccommodation(accommodation);
+  };
+
+  return {
+    isPending,
+    onSubmit,
+  };
+};
