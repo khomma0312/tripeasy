406e3cc26e2a8d8d338efc4f2d748ad2a793f546
khomma0312 <kijikuroneko@gmail.com>
2025-02-06 22:49:08 +0900

    chore: スキーマ、型を追加

--- features/trips/types/index.ts
+++ features/trips/types/index.ts
@@ -1,6 +1,14 @@
-import { tripForDetailSchema, tripForListSchema } from "@/lib/zod/schema/trips";
+import {
+  tripForDetailSchema,
+  tripForListSchema,
+  tripFormSchema,
+} from "@/lib/zod/schema/trips";
 import { z } from "zod";
 
 export type TripForList = z.infer<typeof tripForListSchema>;
 
 export type Trip = z.infer<typeof tripForDetailSchema>;
+
+export type TripFormFieldValues = z.infer<typeof tripFormSchema>;
+
+export type TripFormDefaultValues = Partial<Trip>;
