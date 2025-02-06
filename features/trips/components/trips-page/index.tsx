87ccae5fb7911efd0e217e0cacfef35bc4fd3130
khomma0312 <kijikuroneko@gmail.com>
2025-02-06 22:53:49 +0900

    feat: 旅行情報の登録フォームモーダルのボタンを一覧ページに配置

--- features/trips/components/trips-page/index.tsx
+++ features/trips/components/trips-page/index.tsx
@@ -3,8 +3,9 @@
 import { TripForList } from "@/features/trips/types";
 import { WithPaginationLayout } from "@/components/layout/with-pagination-layout";
 import { CardListColumn } from "@/components/layout/card-list-column";
-import { ListPageHeader } from "@/components/shared/list-page-header";
 import { TripCard } from "@/features/trips/components/trip-card";
+import { RegisterButtonWithFormDialog } from "@/components/shared/register-button-with-form-dialog";
+import { TitleHeading } from "@/components/shared/title-heading";
 
 type Props = {
   trips: TripForList[];
@@ -20,11 +21,10 @@ export const TripsPage = ({ trips, currentPage, totalPages }: Props) => {
       totalPages={totalPages}
     >
       <div>
-        <ListPageHeader
-          title="旅程一覧"
-          labelForButton="旅程を登録"
-          linkForButton="/trips/new"
-        />
+        <div className="flex justify-between items-center mb-8">
+          <TitleHeading>旅程一覧</TitleHeading>
+          <RegisterButtonWithFormDialog dialogTitle="スケジュールを作成" />
+        </div>
         <CardListColumn>
           {trips.map((trip) => (
             <TripCard key={trip.id} trip={trip} />
