c0a00897f2b7970e078ec45ae43da6178d5a2959
khomma0312 <kijikuroneko@gmail.com>
2025-02-17 07:22:38 +0900

    feat: 旅程のタイムラインを表示するサイドバーを追加

---
+++ features/trips/components/itinerary-day-tab-group/index.tsx
@@ -0,0 +1,24 @@
+import { TripDay } from "@/features/trips/types";
+import { format } from "date-fns";
+import { ItineraryDayTab } from "../itinerary-day-tab";
+
+type Props = {
+  tripDays: TripDay[];
+};
+
+export const ItineraryDayTabGroup = ({ tripDays }: Props) => {
+  return (
+    <div className="bg-white text-gray-900 border-b">
+      <div className="flex overflow-x-scroll">
+        <ItineraryDayTab title="概要" />
+        {tripDays.map((tripDay) => (
+          <ItineraryDayTab
+            key={tripDay.dayOrder}
+            title={format(new Date(tripDay.dayDate), "M月d日")}
+            dayOrder={tripDay.dayOrder}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
---
+++ features/trips/components/itinerary-side-bar-header/index.tsx
@@ -0,0 +1,45 @@
+import { Trip } from "@/features/trips/types";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/shadcn/button";
+import { ArrowLeft } from "lucide-react";
+import { Edit2 } from "lucide-react";
+import { format } from "date-fns";
+
+type Props = {
+  trip: Trip;
+};
+
+export const ItinerarySideBarHeader = ({ trip }: Props) => {
+  const router = useRouter();
+
+  return (
+    <div className="bg-gray-900 text-white px-4 py-2">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center space-x-4">
+          <Button
+            variant="ghost"
+            size="icon"
+            className="text-white hover:text-black"
+            onClick={() => router.back()}
+          >
+            <ArrowLeft className="h-6 w-6" />
+          </Button>
+          <h1 className="text-xl font-bold">{trip.title}</h1>
+        </div>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="text-white hover:text-black"
+        >
+          <Edit2 className="h-6 w-6" />
+        </Button>
+      </div>
+      <div className="flex justify-between items-center py-2">
+        <p className="text-sm font-semibold">
+          {format(new Date(trip.startDate), "yyyy/MM/dd")} -{" "}
+          {format(new Date(trip.endDate), "yyyy/MM/dd")}
+        </p>
+      </div>
+    </div>
+  );
+};
