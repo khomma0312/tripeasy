5767af3523aa2a50dbf0dad8335033efa4e37331
khomma0312 <kijikuroneko@gmail.com>
2025-02-26 23:57:12 +0900

    feat: 候補地検索用のAPIを追加

--- services/api/externals/server/google-maps/fetcher.ts
+++ services/api/externals/server/google-maps/fetcher.ts
@@ -1,9 +1,24 @@
-import { Client } from "@googlemaps/google-maps-services-js";
+import {
+  Client,
+  GeocodeRequest,
+  Language,
+  PlaceDetailsRequest,
+  TextSearchRequest,
+} from "@googlemaps/google-maps-services-js";
+
+const SELECTED_FIELDS = [
+  "formatted_address",
+  "geometry",
+  "name",
+  "place_id",
+  "icon",
+  "photos",
+];
 
 const client = new Client();
 
 export const getLatLngFromAddress = async (address: string) => {
-  const args = {
+  const args: GeocodeRequest = {
     params: {
       key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
       address,
@@ -26,4 +41,56 @@ export const getLatLngFromAddress = async (address: string) => {
     lng: res.results[0].geometry.location.lng,
     isLocationReliable,
   };
+};
+
+export const getPlaceDetails = async (placeId: string) => {
+  const args: PlaceDetailsRequest = {
+    params: {
+      key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+      place_id: placeId,
+      fields: SELECTED_FIELDS,
+      language: Language.ja,
+    },
+  };
+  const res = await client.placeDetails(args).then((res) => res.data);
+
+  if (res.status.toLowerCase() !== "ok") {
+    throw new Error(res.error_message);
+  }
+
+  return {
+    places: [res.result],
+    nextPageToken: undefined,
+  };
+};
+
+export const getPlaceSuggestions = async (
+  query: string,
+  nextPageToken?: string
+) => {
+  const args: TextSearchRequest = {
+    params: {
+      key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+      query,
+      language: Language.ja,
+      pagetoken: nextPageToken,
+    },
+  };
+
+  const res = await client.textSearch(args).then((res) => res.data);
+
+  if (res.status.toLowerCase() !== "ok") {
+    throw new Error(res.error_message);
+  }
+
+  return {
+    places: res.results,
+    nextPageToken: res.next_page_token,
+    error: res.status.toLowerCase() !== "ok" ? res.error_message : undefined,
+  };
+};
+
+export const getPlacePhoto = (photoReference: string) => {
+  const url = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${photoReference}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`;
+  return url;
 };
