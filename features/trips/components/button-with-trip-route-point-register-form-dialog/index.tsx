5b399f0bd0497acfe7a42e19cc99b10099c9b5c4
khomma0312 <kijikuroneko@gmail.com>
2025-03-01 23:40:50 +0900

    fix: フォーム入りダイアログを表示するボタンの既存利用部分を修正

---
+++ features/trips/components/button-with-trip-route-point-register-form-dialog/index.tsx
@@ -0,0 +1,56 @@
+import {
+  ButtonWithFormDialog,
+  CloseButton,
+} from "@/components/shared/button-with-form-dialog";
+import { useAddTripRoutePoint } from "@/features/trips/hooks/use-add-trip-route-point";
+import { TripRoutePointForm } from "@/features/trips/components/trip-route-point-form";
+import { useTripRoutePointForm } from "../../hooks/use-trip-route-point-form";
+import { useParams } from "next/navigation";
+import { useSelectedTripDayIdForRegisterAtomValue } from "../../store/selected-tripDayId-for-register";
+
+type Props = {
+  className?: string;
+  name: string;
+  address: string;
+  latLng: { lat: number; lng: number } | undefined;
+  imageUrl: string | undefined;
+  buttonSize?: "sm" | "default" | "lg" | "icon" | null;
+};
+
+export const ButtonWithTripRoutePointRegisterFormDialog = ({
+  className,
+  name,
+  address,
+  latLng,
+  imageUrl,
+  buttonSize,
+}: Props) => {
+  const { id } = useParams();
+  const selectedTripDayId = useSelectedTripDayIdForRegisterAtomValue();
+  const { form } = useTripRoutePointForm(
+    name,
+    address,
+    latLng,
+    imageUrl,
+    selectedTripDayId
+  );
+  const { isPending, onSubmit } = useAddTripRoutePoint(form, Number(id));
+
+  return (
+    <ButtonWithFormDialog
+      buttonLabel="旅程表に追加"
+      dialogTitle="旅程表に追加"
+      dialogDescription="旅行地点に関する情報を入力してください。"
+      className={className}
+      buttonSize={buttonSize}
+      Form={
+        <TripRoutePointForm
+          form={form}
+          isPending={isPending}
+          onSubmit={onSubmit}
+          CloseButton={CloseButton}
+        />
+      }
+    />
+  );
+};
--- features/trips/components/trips-page/index.tsx
+++ features/trips/components/trips-page/index.tsx
@@ -4,7 +4,7 @@ import { TripForList } from "@/features/trips/types";
 import { WithPaginationLayout } from "@/components/layout/with-pagination-layout";
 import { CardListColumn } from "@/components/layout/card-list-column";
 import { TripCard } from "@/features/trips/components/trip-card";
-import { RegisterButtonWithFormDialog } from "@/components/shared/register-button-with-form-dialog";
+import { ButtonWithTripRegisterFormDialog } from "@/features/trips/components/button-with-trip-register-form-dialog";
 import { TitleHeading } from "@/components/shared/title-heading";
 
 type Props = {
@@ -23,7 +23,7 @@ export const TripsPage = ({ trips, currentPage, totalPages }: Props) => {
       <div>
         <div className="flex justify-between items-center mb-8">
           <TitleHeading>旅程一覧</TitleHeading>
-          <RegisterButtonWithFormDialog dialogTitle="スケジュールを作成" />
+          <ButtonWithTripRegisterFormDialog />
         </div>
         <CardListColumn>
           {trips.map((trip) => (
