6818f178ab82efaddbf1c8c2cf976d08764757f2
khomma0312 <kijikuroneko@gmail.com>
2025-03-06 07:25:54 +0900

    feat: 旅行地点を追加するフォームを実装

---
+++ features/trips/components/trip-route-point-form/index.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { Form } from "@/components/shadcn/form";
+import { RHFInputField } from "@/components/shared/rhf-input-field";
+import { TripRoutePointFormFieldValues } from "@/features/trips/types";
+import { UseFormReturn } from "react-hook-form";
+import { Button } from "@/components/shadcn/button";
+import { RHFTimeSelectField } from "@/components/shared/rhf-time-select-field";
+
+type Props = {
+  form: UseFormReturn<TripRoutePointFormFieldValues>;
+  isPending: boolean;
+  onSubmit: (trip: TripRoutePointFormFieldValues) => void;
+  RegisterButton?: React.ComponentType;
+  CloseButton?: React.ComponentType;
+};
+
+export const TripRoutePointForm = ({
+  form,
+  onSubmit,
+  RegisterButton,
+  CloseButton,
+}: Props) => {
+  return (
+    <div className="h-full">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-6 bg-white p-6"
+        >
+          <div className="grid grid-cols-1 gap-6">
+            <div className="space-y-2">
+              <RHFInputField
+                control={form.control}
+                name="name"
+                label="目的地名"
+                isRequired
+              />
+            </div>
+            <div className="space-y-2">
+              <RHFInputField
+                control={form.control}
+                name="address"
+                label="住所"
+                isRequired
+              />
+            </div>
+            <div className="flex items-center gap-2">
+              <RHFTimeSelectField
+                control={form.control}
+                name="arrivalTime"
+                label="到着時間"
+                defaultHour="9"
+                defaultMinute="0"
+                isRequired
+                onChange={(hour, minute) => {
+                  form.setValue("arrivalTime", `${hour}:${minute}`);
+                }}
+              />
+              <RHFTimeSelectField
+                control={form.control}
+                name="departureTime"
+                label="出発時間"
+                defaultHour="10"
+                defaultMinute="0"
+                isRequired
+                onChange={(hour, minute) => {
+                  form.setValue("departureTime", `${hour}:${minute}`);
+                }}
+              />
+            </div>
+          </div>
+          <div className="flex justify-end gap-2">
+            {RegisterButton ? (
+              <RegisterButton />
+            ) : (
+              <Button type="submit">作成する</Button>
+            )}
+            {CloseButton ? (
+              <CloseButton />
+            ) : (
+              <Button variant="outline">キャンセル</Button>
+            )}
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+};
---
+++ features/trips/hooks/use-add-trip-route-point.ts
@@ -0,0 +1,43 @@
+import { useQueryClient } from "@tanstack/react-query";
+import { useErrorToast } from "@/hooks/common/use-error-toast";
+import { useToast } from "@/hooks/shadcn/use-toast";
+import { usePostTripRoutePoints } from "@/services/api/endpoints/trip-route-points/trip-route-points";
+import { TripRoutePointFormFieldValues } from "@/features/trips/types";
+import { UseFormReturn } from "react-hook-form";
+import { useRef } from "react";
+import { generateRandomKey } from "@/utils/common";
+
+export const useAddTripRoutePoint = (
+  form: UseFormReturn<TripRoutePointFormFieldValues>,
+  tripId: number
+) => {
+  const formKeyRef = useRef(generateRandomKey());
+  const { toast } = useToast();
+  const { errorToast } = useErrorToast();
+  const queryClient = useQueryClient();
+  const { isPending, mutate } = usePostTripRoutePoints({
+    mutation: {
+      onSuccess: () => {
+        toast({ title: "旅行地点を追加しました" });
+        queryClient.invalidateQueries({
+          queryKey: [`/api/trips/${tripId}`],
+        });
+        formKeyRef.current = generateRandomKey();
+        form.reset();
+      },
+      onError: () => {
+        errorToast("旅行地点の追加に失敗しました");
+      },
+    },
+  });
+
+  const onSubmit = (tripRoutePoint: TripRoutePointFormFieldValues) => {
+    mutate({
+      data: {
+        tripRoutePoint,
+      },
+    });
+  };
+
+  return { isPending, onSubmit, formKey: formKeyRef.current };
+};
