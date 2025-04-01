2f132ec0f4207b3389bc638b5d09ec2c50b6ee46
khomma0312 <kijikuroneko@gmail.com>
2025-04-02 08:33:51 +0900

    feat: 旅行地点を追加するために各APIを修正

--- app/api/[...route]/trip-route-points.ts
+++ app/api/[...route]/trip-route-points.ts
@@ -1,9 +1,4 @@
 import { Hono } from "hono";
-import { db } from "@/lib/drizzle/db";
-import {
-  tripRoutePoints as tripRoutePointsTable,
-  destinations as destinationsTable,
-} from "@/schema";
 import { getLogger } from "@/lib/logger";
 import { auth } from "@/lib/auth";
 import { ApiErrorType } from "@/lib/zod/schema/common";
@@ -12,9 +7,14 @@ import {
   apiPostInputSchema,
   ApiPostOutputType,
 } from "@/lib/zod/schema/trip-route-points";
-import { eq, inArray, sql } from "drizzle-orm";
 import { getLatLngFromAddress } from "@/services/api/externals/server/google-maps/fetcher";
-import { convertTimeToDate } from "@/features/trips/utils";
+import {
+  addNewAccommodation,
+  addNewDestination,
+  addNewTripRoutePoint,
+  calculateNewRoutePointVisitOrder,
+  updateTripRoutePointVisitOrder,
+} from "@/utils/api/trip-route-points";
 
 const logger = getLogger("api/trip-route-points");
 
@@ -24,19 +24,7 @@ const app = new Hono().post(
   async (c) => {
     const session = await auth();
     const { tripRoutePoint } = c.req.valid("json");
-    const {
-      name,
-      address,
-      latLng: latLngInput,
-      imageUrl,
-      arrivalTime,
-      departureTime,
-      tripDayId,
-    } = tripRoutePoint;
-
-    let latLng: { x: number; y: number } | null = latLngInput
-      ? { x: latLngInput.lng, y: latLngInput.lat }
-      : null;
+    const { destination, accommodation } = tripRoutePoint;
 
     if (!session?.user) {
       logger.error("User verification failed.");
@@ -48,110 +36,125 @@ const app = new Hono().post(
 
     const userId = session.user.id ?? "";
 
-    try {
-      // 住所から緯度経度を取得
-      const { lat, lng, isLocationReliable } = await getLatLngFromAddress(
-        address
-      );
-      if (isLocationReliable && !latLng) latLng = { x: lng, y: lat };
-    } catch (e) {
-      logger.error(e);
-    }
+    // destinationがinputにある場合
+    if (destination) {
+      const {
+        name,
+        address,
+        latLng: latLngInput,
+        imageUrl,
+        tripDayId,
+        arrivalTime,
+        departureTime,
+      } = destination;
 
-    try {
-      const savedTripRoutePoints = await db
-        .select()
-        .from(tripRoutePointsTable)
-        .where(eq(tripRoutePointsTable.tripDaySegmentId, tripDayId));
+      let latLng: { x: number; y: number } | undefined = latLngInput
+        ? { x: latLngInput.lng, y: latLngInput.lat }
+        : undefined;
 
-      const tripRoutePointVisitOrders = savedTripRoutePoints.map(
-        (tripRoutePoint) => ({
-          id: tripRoutePoint.id,
-          arrivalTime: convertTimeToDate(
-            new Date(),
-            tripRoutePoint.arrivalTime
-          ),
-          visitOrder: tripRoutePoint.visitOrder,
-        })
-      );
-
-      const newTripRoutePoint = {
-        id: null,
-        arrivalTime: convertTimeToDate(new Date(), `${arrivalTime}:00`),
-      };
-
-      // 新規tripRoutePointを追加して、arrivalTimeの早い順にvisitOrderを振り直す
-      const sortedTripRoutePointVisitOrders = [
-        newTripRoutePoint,
-        ...tripRoutePointVisitOrders,
-      ]
-        .sort((a, b) => a.arrivalTime.getTime() - b.arrivalTime.getTime())
-        .map((tripRoutePoint, index) => ({
-          ...tripRoutePoint,
-          visitOrder: index + 1,
-        }));
-
-      const newVisitOrder = sortedTripRoutePointVisitOrders.find(
-        (tripRoutePoint) => tripRoutePoint.id === null
-      )?.visitOrder;
-
-      if (!newVisitOrder) {
-        throw new Error("visitOrderが取得できませんでした");
+      try {
+        // 住所から緯度経度を取得
+        const { lat, lng, isLocationReliable } = await getLatLngFromAddress(
+          address
+        );
+        if (isLocationReliable && !latLng) latLng = { x: lng, y: lat };
+      } catch (e) {
+        // 緯度経度取得に失敗した場合はlatLngをundefinedにしてログ出力
+        logger.error(e);
       }
 
-      // name, address, latLngからdestinationを作成
-      const [addedDestination] = await db
-        .insert(destinationsTable)
-        .values({
-          name,
-          address,
-          latLng,
-          imageUrl,
-          userId,
-        })
-        .returning({ id: destinationsTable.id });
+      try {
+        const { newVisitOrder, sortedTripRoutePointVisitOrders } =
+          await calculateNewRoutePointVisitOrder(tripDayId, arrivalTime);
 
-      const [addedTripRoutePoint] = await db
-        .insert(tripRoutePointsTable)
-        .values({
+        // name, address, latLngからdestinationを作成
+        const addedDestination = await addNewDestination(
+          {
+            name,
+            address,
+            latLng,
+            imageUrl,
+          },
+          userId
+        );
+
+        const newTripRoutePoint = {
           arrivalTime,
           departureTime,
           userId,
           destinationId: addedDestination.id,
           tripDaySegmentId: tripDayId,
           visitOrder: newVisitOrder,
-        })
-        .returning({ id: tripRoutePointsTable.id });
+        };
 
-      const updatedVisitOrders = sortedTripRoutePointVisitOrders.filter(
-        (point) => point.id !== null
-      );
+        const addedTripRoutePoint = await addNewTripRoutePoint(
+          newTripRoutePoint
+        );
 
-      if (updatedVisitOrders.length > 0) {
-        const ids = updatedVisitOrders.map((item) => item.id);
+        await updateTripRoutePointVisitOrder(sortedTripRoutePointVisitOrders);
 
-        // 既存のvisitOrderを更新する
-        let caseExpression = sql`CASE `;
-        updatedVisitOrders.forEach((item) => {
-          caseExpression = sql`${caseExpression} WHEN ${tripRoutePointsTable.id} = ${item.id} THEN ${item.visitOrder}`;
-        });
-        caseExpression = sql`${caseExpression} ELSE ${tripRoutePointsTable.visitOrder} END`;
-        await db
-          .update(tripRoutePointsTable)
-          .set({
-            visitOrder: caseExpression,
-          })
-          .where(inArray(tripRoutePointsTable.id, ids));
+        return c.json<ApiPostOutputType>({ id: addedTripRoutePoint.id });
+      } catch (e) {
+        logger.error(e);
+        return c.json<ApiErrorType>(
+          { message: "データの登録に失敗しました" },
+          500
+        );
       }
-
-      return c.json<ApiPostOutputType>({ id: addedTripRoutePoint.id });
-    } catch (e) {
-      logger.error(e);
-      return c.json<ApiErrorType>(
-        { message: "データの登録に失敗しました" },
-        500
-      );
     }
+
+    // accommodationがinputにある場合
+    if (accommodation) {
+      try {
+        let accommodationId: number | undefined = undefined;
+        const {
+          arrivalTime,
+          departureTime,
+          tripDayId,
+          accommodationId: accommodationIdInput,
+        } = accommodation;
+
+        if (accommodationIdInput) {
+          accommodationId = accommodationIdInput;
+        } else {
+          // name, address, latLngからdestinationを作成
+          const addedAccommodation = await addNewAccommodation(
+            accommodation,
+            userId
+          );
+          accommodationId = addedAccommodation.id;
+        }
+
+        const { newVisitOrder, sortedTripRoutePointVisitOrders } =
+          await calculateNewRoutePointVisitOrder(tripDayId, arrivalTime);
+
+        const newTripRoutePoint = {
+          arrivalTime,
+          departureTime,
+          userId,
+          accommodationId,
+          tripDaySegmentId: tripDayId,
+          visitOrder: newVisitOrder,
+        };
+
+        const addedTripRoutePoint = await addNewTripRoutePoint(
+          newTripRoutePoint
+        );
+
+        await updateTripRoutePointVisitOrder(sortedTripRoutePointVisitOrders);
+
+        return c.json<ApiPostOutputType>({ id: addedTripRoutePoint.id });
+      } catch (e) {
+        logger.error(e);
+        return c.json<ApiErrorType>(
+          { message: "データの登録に失敗しました" },
+          500
+        );
+      }
+    }
+
+    logger.error("destinationまたはaccommodationが存在しません");
+    return c.json<ApiErrorType>({ message: "データの登録に失敗しました" }, 500);
   }
 );
 
