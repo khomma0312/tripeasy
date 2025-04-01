7e4beeb191f266005ccc3dff658d33c3684df9e1
khomma0312 <kijikuroneko@gmail.com>
2025-04-02 08:53:03 +0900

    feat: スクロールして別の日付が中心に近づいた際、自動的に別の日付のデータがマップに反映されるよう修正

--- features/trips/components/itinerary-timeline-section/index.tsx
+++ features/trips/components/itinerary-timeline-section/index.tsx
@@ -5,18 +5,54 @@ import { ja } from "date-fns/locale";
 import { ItineraryTripRoutePointCard } from "@/features/trips/components/itinerary-trip-route-point-card";
 import { TripRoutePointAddButtonGroup } from "@/features/trips/components/trip-route-point-add-button-group";
 import { cn } from "@/utils/common";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+import { useSelectedTripDayIndexAtom } from "@/features/trips/store/selected-trip-day-index";
+import { getTripDayIndex } from "@/features/trips/utils";
 
 type Props = {
   tripDay: TripDay;
+  tripDays: TripDay[];
 };
 
-export const ItineraryTimelineSection = ({ tripDay }: Props) => {
-  const { dayOrder, dayDate, tripRoutePoints } = tripDay;
+export const ItineraryTimelineSection = ({ tripDay, tripDays }: Props) => {
+  const { ref, entry } = useInView({
+    rootMargin: "-20% 0px -50% 0px", // より狭い範囲で判定
+  });
+  const [selectedTripDayIndex, setSelectedTripDayIndex] =
+    useSelectedTripDayIndexAtom();
+
+  const hasCurrentDayTripRoutePoints =
+    tripDay?.tripRoutePoints?.length ?? 0 > 0;
+
+  const hasNextDayTripRoutePoints =
+    tripDays[selectedTripDayIndex ? selectedTripDayIndex + 1 : 0]
+      ?.tripRoutePoints?.length ?? 0 > 0;
+  const isVisible = entry?.isIntersecting || false;
+
+  useEffect(() => {
+    if (
+      isVisible &&
+      (hasCurrentDayTripRoutePoints || hasNextDayTripRoutePoints)
+    ) {
+      const index = getTripDayIndex(tripDay.dayOrder);
+      setSelectedTripDayIndex(index);
+    }
+  }, [
+    isVisible,
+    setSelectedTripDayIndex,
+    tripDay.dayOrder,
+    hasCurrentDayTripRoutePoints,
+    hasNextDayTripRoutePoints,
+  ]);
+
+  const { dayOrder, dayDate, tripDayId, tripRoutePoints } = tripDay;
   const colorIndex = (dayOrder - 1) % colorNames.length;
   const itineraryDayDate = new Date(dayDate);
 
   return (
     <div className="flex flex-col gap-4 p-4">
+      <div ref={ref} />
       {/* ヘッダー部分 */}
       <div className="flex justify-between">
         <h4
@@ -33,7 +69,7 @@ export const ItineraryTimelineSection = ({ tripDay }: Props) => {
       </div>
       {/* コンテンツ部分 */}
       <div className="flex flex-col gap-3">
-        {tripRoutePoints.map((tripRoutePoint) => (
+        {tripRoutePoints?.map((tripRoutePoint) => (
           <ItineraryTripRoutePointCard
             key={tripRoutePoint.visitOrder}
             tripRoutePoint={tripRoutePoint}
@@ -42,7 +78,7 @@ export const ItineraryTimelineSection = ({ tripDay }: Props) => {
         ))}
       </div>
       {/* 予定追加ボタン部分 */}
-      <TripRoutePointAddButtonGroup />
+      <TripRoutePointAddButtonGroup tripDayId={tripDayId} />
     </div>
   );
 };
