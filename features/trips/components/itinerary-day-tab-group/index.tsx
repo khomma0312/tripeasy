6e694c19910817488dfbecf50ae17dfa38b734bd
khomma0312 <kijikuroneko@gmail.com>
2025-04-02 08:53:39 +0900

    fix: 各レイアウトを修正

--- features/trips/components/itinerary-day-tab-group/index.tsx
+++ features/trips/components/itinerary-day-tab-group/index.tsx
@@ -8,17 +8,15 @@ type Props = {
 
 export const ItineraryDayTabGroup = ({ tripDays }: Props) => {
   return (
-    <div className="bg-white text-gray-900 border-b">
-      <div className="flex overflow-x-scroll">
-        <ItineraryDayTab title="概要" />
-        {tripDays.map((tripDay) => (
-          <ItineraryDayTab
-            key={tripDay.dayOrder}
-            title={format(new Date(tripDay.dayDate), "M月d日")}
-            dayOrder={tripDay.dayOrder}
-          />
-        ))}
-      </div>
+    <div className="bg-white text-gray-900 border-b flex overflow-x-scroll">
+      <ItineraryDayTab title="概要" />
+      {tripDays.map((tripDay) => (
+        <ItineraryDayTab
+          key={tripDay.dayOrder}
+          title={format(new Date(tripDay.dayDate), "M月d日")}
+          dayOrder={tripDay.dayOrder}
+        />
+      ))}
     </div>
   );
 };
