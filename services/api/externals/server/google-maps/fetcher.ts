c766aca13da6fdf4fd7b43978e20053044d09689
khomma0312 <kijikuroneko@gmail.com>
2025-04-02 08:35:47 +0900

    feat: 緯度経度指定での検索と文字列指定での検索どちらにも対応できるように関数を修正

--- services/api/externals/server/google-maps/fetcher.ts
+++ services/api/externals/server/google-maps/fetcher.ts
@@ -3,8 +3,12 @@ import {
   GeocodeRequest,
   Language,
   PlaceDetailsRequest,
+  PlaceType1,
   TextSearchRequest,
+  PlacesNearbyRequest,
+  ReverseGeocodeRequest,
 } from "@googlemaps/google-maps-services-js";
+import { TripRoutePointPlaceType } from "@/features/trips/types";
 
 const SELECTED_FIELDS = [
   "formatted_address",
@@ -14,6 +18,13 @@ const SELECTED_FIELDS = [
   "icon",
   "photos",
 ];
+
+const searchTypeToPlaceTypeMap: {
+  [key in TripRoutePointPlaceType]: PlaceType1 | undefined;
+} = {
+  accommodation: PlaceType1.lodging,
+  destination: undefined,
+};
 
 const client = new Client();
 
@@ -43,6 +54,35 @@ export const getLatLngFromAddress = async (address: string) => {
   };
 };
 
+export const getAddressFromLatLng = async (latLng: {
+  lat: number;
+  lng: number;
+}) => {
+  const args: ReverseGeocodeRequest = {
+    params: {
+      key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+      latlng: `${latLng.lat},${latLng.lng}`,
+      language: Language.ja,
+    },
+  };
+  const res = await client.reverseGeocode(args).then((res) => res.data);
+
+  if (res.status.toLowerCase() !== "ok") {
+    throw new Error(res.error_message);
+  }
+
+  const result = res.results[0];
+  // typeにpremiseかsubpremiseが含まれていれば、正確な位置情報として扱う
+  const isLocationReliable = result.address_components[0].types.some(
+    (type) => type === "premise" || type === "subpremise"
+  );
+
+  return {
+    address: result.formatted_address,
+    isLocationReliable,
+  };
+};
+
 export const getPlaceDetails = async (placeId: string) => {
   const args: PlaceDetailsRequest = {
     params: {
@@ -64,9 +104,37 @@ export const getPlaceDetails = async (placeId: string) => {
   };
 };
 
-export const getPlaceSuggestions = async (
+export const getPlaceSuggestions = async (params: {
+  query: string;
+  nextPageToken?: string;
+  searchLocation?: { lat: number; lng: number };
+  searchType?: TripRoutePointPlaceType;
+}) => {
+  const { query, nextPageToken, searchType, searchLocation } = params;
+
+  if (!query) {
+    if (searchLocation) {
+      return getPlaceSuggestionsByLocation(
+        searchLocation,
+        nextPageToken,
+        searchType
+      );
+    }
+
+    return {
+      places: [],
+      nextPageToken: undefined,
+      error: undefined,
+    };
+  }
+
+  return getPlaceSuggestionsByText(query, nextPageToken, searchType);
+};
+
+export const getPlaceSuggestionsByText = async (
   query: string,
-  nextPageToken?: string
+  nextPageToken?: string,
+  searchType?: TripRoutePointPlaceType
 ) => {
   const args: TextSearchRequest = {
     params: {
@@ -74,10 +142,40 @@ export const getPlaceSuggestions = async (
       query,
       language: Language.ja,
       pagetoken: nextPageToken,
+      type: searchType ? searchTypeToPlaceTypeMap[searchType] : undefined,
     },
   };
 
   const res = await client.textSearch(args).then((res) => res.data);
+
+  if (res.status.toLowerCase() !== "ok") {
+    throw new Error(res.error_message);
+  }
+
+  return {
+    places: res.results,
+    nextPageToken: res.next_page_token,
+    error: res.status.toLowerCase() !== "ok" ? res.error_message : undefined,
+  };
+};
+
+export const getPlaceSuggestionsByLocation = async (
+  searchLocation: { lat: number; lng: number },
+  nextPageToken?: string,
+  searchType?: TripRoutePointPlaceType
+) => {
+  const args: PlacesNearbyRequest = {
+    params: {
+      key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+      location: `${searchLocation.lat},${searchLocation.lng}`,
+      radius: 5000,
+      language: Language.ja,
+      pagetoken: nextPageToken,
+      type: searchType ? searchTypeToPlaceTypeMap[searchType] : undefined,
+    },
+  };
+
+  const res = await client.placesNearby(args).then((res) => res.data);
 
   if (res.status.toLowerCase() !== "ok") {
     throw new Error(res.error_message);
