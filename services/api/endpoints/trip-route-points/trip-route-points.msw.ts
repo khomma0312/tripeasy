8eb12b09656a1cd45b0f9650e05204293be51cda
khomma0312 <kijikuroneko@gmail.com>
2025-04-11 07:51:13 +0900

    chore: orvalによる自動生成

--- services/api/endpoints/trip-route-points/trip-route-points.msw.ts
+++ services/api/endpoints/trip-route-points/trip-route-points.msw.ts
@@ -14,10 +14,13 @@ import {
   http
 } from 'msw'
 import type {
+  PatchTripRoutePointsReorder200,
   PostTripRoutePoints200
 } from '../../model'
 
 export const getPostTripRoutePointsResponseMock = (overrideResponse: Partial< PostTripRoutePoints200 > = {}): PostTripRoutePoints200 => ({id: faker.number.int({min: undefined, max: undefined}), ...overrideResponse})
+
+export const getPatchTripRoutePointsReorderResponseMock = (overrideResponse: Partial< PatchTripRoutePointsReorder200 > = {}): PatchTripRoutePointsReorder200 => ({ids: Array.from({ length: faker.number.int({ min: 1, max: 10 }) }, (_, i) => i + 1).map(() => (faker.number.int({min: undefined, max: undefined}))), ...overrideResponse})
 
 
 export const getPostTripRoutePointsMockHandler = (overrideResponse?: PostTripRoutePoints200 | ((info: Parameters<Parameters<typeof http.post>[1]>[0]) => Promise<PostTripRoutePoints200> | PostTripRoutePoints200)) => {
@@ -31,6 +34,19 @@ export const getPostTripRoutePointsMockHandler = (overrideResponse?: PostTripRou
       })
   })
 }
+
+export const getPatchTripRoutePointsReorderMockHandler = (overrideResponse?: PatchTripRoutePointsReorder200 | ((info: Parameters<Parameters<typeof http.patch>[1]>[0]) => Promise<PatchTripRoutePointsReorder200> | PatchTripRoutePointsReorder200)) => {
+  return http.patch('*/trip-route-points/reorder', async (info) => {await delay(1000);
+  
+    return new HttpResponse(JSON.stringify(overrideResponse !== undefined 
+            ? (typeof overrideResponse === "function" ? await overrideResponse(info) : overrideResponse) 
+            : getPatchTripRoutePointsReorderResponseMock()),
+      { status: 200,
+        headers: { 'Content-Type': 'application/json' }
+      })
+  })
+}
 export const getTripRoutePointsMock = () => [
-  getPostTripRoutePointsMockHandler()
+  getPostTripRoutePointsMockHandler(),
+  getPatchTripRoutePointsReorderMockHandler()
 ]
