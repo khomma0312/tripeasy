15d9d63ef4f27dc8572f7e76e84bbf67d5280a2f
khomma0312 <kijikuroneko@gmail.com>
2025-04-16 08:32:42 +0900

    fix:ドラッグ&ドロップ用ボタンのテキストサイズを修正

--- features/trips/components/drag-and-drop-button/index.tsx
+++ features/trips/components/drag-and-drop-button/index.tsx
@@ -11,7 +11,7 @@ export const DragAndDropButton = ({ listeners }: Props) => {
     <div
       {...listeners}
       className={cn(
-        "flex items-center justify-center gap-1 h-8 px-3 rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none cursor-move hover:bg-accent hover:text-accent-foreground"
+        "flex items-center justify-center gap-1 h-8 px-3 rounded-md text-xs font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none cursor-move hover:bg-accent hover:text-accent-foreground"
       )}
     >
       <ArrowDownUp className="size-4" />
